582d945ee01f94d3afc3a2eb6fad3bb7135fe426
radon1819 <106231820+radon1819@users.noreply.github.com>
2024-05-25 08:31:37 +0530

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 # app.py
 import streamlit as st
-from PIL import Image, ImageOps
+from PIL import Image
 import pytesseract
-import cv2
-import numpy as np
 from pytesseract import TesseractNotFoundError
 
 def main():
@@ -18,30 +16,11 @@ def main():
 
         if st.button('Recognize Text'):
             try:
-                preprocessed_image = preprocess_image(image)
-                text = extract_text(preprocessed_image)
+                text = extract_text(image)
                 st.write("Extracted Text:")
                 st.write(text)
             except TesseractNotFoundError:
                 st.error("Tesseract OCR is not installed. Please make sure it's installed and configured correctly.")
-
-def preprocess_image(image):
-    # Convert image to grayscale
-    gray = ImageOps.grayscale(image)
-
-    # Convert image to numpy array
-    img_array = np.array(gray)
-
-    # Apply GaussianBlur to remove noise
-    img_array = cv2.GaussianBlur(img_array, (5, 5), 0)
-
-    # Apply thresholding to get a binary image
-    _, img_array = cv2.threshold(img_array, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    # Convert back to PIL image
-    preprocessed_image = Image.fromarray(img_array)
-
-    return preprocessed_image
 
 def extract_text(image):
     # Convert image to text using Tesseract OCR with custom configuration
